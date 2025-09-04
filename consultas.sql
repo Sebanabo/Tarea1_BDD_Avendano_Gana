ff304cb0f7196c4352727c521ead7f216e1f1001
JavierGanaa <143565756+JavierGanaa@users.noreply.github.com>
2025-09-03 23:12:41 -0400

    Add files via upload
    con consulta 6

--- consultas.sql
+++ consultas.sql
@@ -48,7 +48,22 @@ ON
 	Funcionalidad.Topico = Error.Topico
 
 --6
---Esperando respuesta de los ayudantes
+--Esperando respuesta de los ayudantes     POR LO QUE ESTA SUJETO A CAMBIO
+select * -- Muestra de como eran antes
+from Error
+WHERE 
+    Fecha < '2022-09-03';
+UPDATE 
+    Error -- RECORDAR QUE DEBE SER DE FUNCIONALIDAD, ahora usamos Error como prueba, porque Funcionalidad no tiene atributo Fecha, aunque se lo podemos añadir luego
+SET 
+    Estado = 'Archivado'
+WHERE 
+    Fecha < '2022-09-03'; -- tambien podria ser WHERE YEAR(Fecha) < 2022; aunque esta opcion no tiene en cuenta mes ni dia
+
+select * -- Muestra de que fueron archivados
+from Error
+WHERE 
+    Fecha < '2022-09-03';
 
 --7
 SELECT
